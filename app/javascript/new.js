60c0eab1d575935a77dad1e69cbf2f8686191db3
waku-miyamoto <67049564+waku-miyamoto@users.noreply.github.com>
2020-08-11 09:59:20 +0900

    edit_contens

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,4 +1,7 @@
-$(document).on('turbolinks:load', function(){
+// $(document).on('turbolinks:load', function(){
+$(document).ready(function(){
+
+
   // 下記はedit用です。できれば別ファイルで作成することを推奨。バグの元
   var append_input = $(`<li class="input"><label class="upload-label"><div class="upload-label__text">ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード<div class="input-area"><input class="hidden image_upload" type="file"></div></div></label></li>`)
   $ul = $('#previews')
