e012ea706ab7bed4a6dac7cbef5c124f1f2ab687
takumi-19 <tkm19.k.univ@icloud.com>
2020-08-11 10:31:50 +0900

    change name auction to buy

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   has_one :address
   has_one :card
   has_many :saling_items, -> { where("seller_id is not NULL && buyer_id is NULL") }, class_name: "Product"
-  has_many :sold_items, -> { where("seller_id is not NULL && buyer_id is not NULL && auction_id is NULL") }, class_name: "Product"
-  has_many :auction_items, -> { where("seller_id is not NULL && auction_id is not NULL && buyer_id is NULL") }, class_name: "Product"
+  has_many :sold_items, -> { where("seller_id is not NULL && buyer_id is not NULL") }, class_name: "Product"
+  # has_many :auction_items, -> { where("seller_id is not NULL && auction_id is not NULL && buyer_id is NULL") }, class_name: "Product"
   validates :family_name, presence: true
   validates :first_name, presence: true
   validates :family_name_kana, presence: true
