ff98021aec8c85ca8f7b51c52cfd8e440e8af5f7
Fuku-Taka <67095267+Fuku-Taka@users.noreply.github.com>
2022-05-05 21:16:17 +0900

    unicornバージョン指定〜bundle_install

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,2 @@
-server '54.248.243.225', user: 'ec2-user', roles: %w{app db web}
+# server '54.248.243.225', user: 'ec2-user', roles: %w{app db web}
+server '52.196.248.97', user: 'ec2-user', roles: %w{app db web}
