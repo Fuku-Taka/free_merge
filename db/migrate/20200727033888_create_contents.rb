65a02459bfb452ef847b2bea79e6a2b925fdeec4
Fuku-Taka <67095267+Fuku-Taka@users.noreply.github.com>
2022-05-19 20:58:35 +0900

    formatting

--- db/migrate/20200727033888_create_contents.rb
+++ db/migrate/20200727033888_create_contents.rb
@@ -4,16 +4,16 @@ class CreateContents < ActiveRecord::Migration[6.0]
       t.references :seller, foreign_key: {to_table: :users}, null: false
       t.references :buyer, foreign_key: {to_table: :users}
       t.references :auction, foreign_key: {to_table: :users}
-      t.string :name, null: false
-      t.integer :price, null: false
-      t.text :explain, null: false
-      t.integer :status, null: false
-      t.integer :size, null: false
-      t.integer :prefecture, null: false
-      t.integer :postage, null: false
-      t.integer :shipment, null: false
-      t.string :brand
-      t.integer :category_id
+      t.string     :name, null: false
+      t.integer    :price, null: false
+      t.text       :explain, null: false
+      t.integer    :status, null: false
+      t.integer    :size, null: false
+      t.integer    :prefecture, null: false
+      t.integer    :postage, null: false
+      t.integer    :shipment, null: false
+      t.string     :brand
+      t.integer    :category_id
       t.timestamps
     end
   end
